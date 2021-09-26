40bc12e8bac311f6c1bd317dcb36a6467e0e0298
mcmusonda1 <mcmusondaleonard@gmail.com>
2021-09-26 22:25:26 +0300

    This is conditional statement, for party attendance.

---
+++ src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Are you eligible to attend this part?? ");
+        System.out.println("How old are you?");
+
+        //input
+        int age = scan.nextInt();
+        System.out.println();
+
+        //Condition
+
+
+        if (age >= 0 && age <= 17) {
+            System.out.println("You are too young for the party, go and play somewhere else");
+        } else if (age >= 18 && age <= 40) {
+            System.out.println("You are eligible to attend the party, go and have fun!!!!");
+        } else if (age >= 41 && age <= 100) {
+            System.out.println("You are old, go and have some rest");
+        } else {
+            System.out.println("Invalid input");
+        }
+
+        //Switch case
+        int year = (20);
+        switch (year) {
+            case 20 -> System.out.println("Happy 20th birthday");
+            case 21 -> System.out.println("You are beyond 20");
+            default -> System.out.println("Invalid input");
+        }
+
+    }
+}
