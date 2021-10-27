135e2c54b0ded2487daaeb7c1017854c59e313e2
mcmusonda1 <mcmusondaleonard@gmail.com>
2021-10-27 15:34:52 +0300

    Grading System

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -50,5 +50,30 @@ public class Main {
         {
             System.out.println(number);
         }
+//        Grading system
+            Scanner grade = new Scanner(System.in);
+            System.out.println("Enter your marks: ");
+
+            int marks = grade.nextInt();
+
+            if(marks<50){
+                System.out.println("fail");
+            }
+            else if(marks>=50 && marks<60){
+                System.out.println("D grade");
+            }
+            else if(marks>=60 && marks<70){
+                System.out.println("C grade");
+            }
+            else if(marks>=70 && marks<80){
+                System.out.println("B grade");
+            }
+            else if(marks>=80 && marks<90){
+                System.out.println("A grade");
+            }else if(marks>=90 && marks<100){
+                System.out.println("A+ grade");
+            }else{
+                System.out.println("Invalid!");
+            }
     }
 }
